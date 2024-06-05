08e66bafcd13ad7f09f7c91c5be07d1b8ffce69b
fikayo <fikayoadele@gmail.com>
2024-06-05 10:31:29 +0100

    final

--- src/pages/testimonials/testimonial.jsx
+++ src/pages/testimonials/testimonial.jsx
@@ -9,21 +9,21 @@ const Testimonial = () => {
                 <img className='priImg' src='https://www.brides.com/thmb/2s9wOA7P7orQk_hB4ZxOKhb3UaQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Kristin-Wyatt-square_ShannonSklossPhotography-112-6c32c58833e346e59beab690620850b3.jpg' alt='' />
                 <div className='lCont'><img className='love' src='./love.png'/><h1>Sarah  +  Mike</h1></div>
                 <p>
-                When Sarah and Michael entrusted Yulia with capturing the most precious moments of their wedding day, 
+                When Sarah and Michael entrusted Hd photography with capturing the most precious moments of their wedding day, 
                 little did they know they were embarking on a journey filled with magic, laughter, and pure enchantment.
-                 From the first click of his camera to the final frame, Yulia's passion for his craft and unwavering 
+                 From the first click of his camera to the final frame, Hd photography's passion for his craft and unwavering 
                  commitment to excellence shone brightly, leaving Sarah and Michael in awe of his extraordinary talent.
                  </p>
 
                  <p>
                 As the sun dipped below the horizon and bathed the world in a golden glow, 
-                Yulia worked his magic, weaving a tapestry of memories that would last a lifetime. With each click of the shutter,
+                Hd photography worked his magic, weaving a tapestry of memories that would last a lifetime. With each click of the shutter,
                  he captured not just photographs, but fleeting moments of love and tenderness – the stolen glances, the gentle caresses,
                   the unspoken promises exchanged between two souls deeply in love.
                   </p>
                   <p>
                 When Sarah and Michael received their wedding album, they were met with a treasure trove of memories – a visual symphony of love,
-                 laughter, and joy that transported them back to that magical day. Each photograph was a testament to Yulia's unparalleled skill and artistry,
+                 laughter, and joy that transported them back to that magical day. Each photograph was a testament to Hd photography's unparalleled skill and artistry,
                   a testament to his ability to turn fleeting moments into timeless treasures that would be cherished for generations to come.
                   </p>
             </div>
@@ -32,9 +32,9 @@ const Testimonial = () => {
                 <img className='priImg' src='https://www.brides.com/thmb/ubzVmCRKtJ0_TI4w955rsmOCT8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/recirc-blue-france-wedding-couple-portrait-jenny-quicksall-1123-d5995b4fa7d64ceb86fbcf13d022f929.jpg' alt='' />
                 <div className='lCont'><img className='love' src='./love.png'/><h1>Emily  +  David</h1></div>
                 <p>When Emily and David embarked on their journey towards forever, they knew they wanted someone special to capture the essence of their love – and they found that 
-                    and more in Yulia's Photography.</p> 
+                    and more in Hd photography's Photography.</p> 
                     <p> From the first consultation to the final delivery of their wedding album,
-                     Yulia exceeded every expectation, turning their most cherished moments into timeless works of art.</p>
+                     Hd photography exceeded every expectation, turning their most cherished moments into timeless works of art.</p>
                 <p>When Emily and David flipped through their wedding album for the first time, they were transported back to that magical day – the soft whispers, the gentle touches, the laughter that filled the air.</p>
             </div>
             
@@ -45,9 +45,9 @@ const Testimonial = () => {
                     photographer was paramount.
                     Little did they know that they would not only find a photographer but also a storyteller, a magician who 
                     would turn their cherished moments into timeless treasures.</p>
-                    <p> Enter Yulia's Photography – a beacon of creativity, professionalism, and unwavering dedication.
+                    <p> Enter Hd photography's Photography – a beacon of creativity, professionalism, and unwavering dedication.
                     From the intimate exchanges of vows to the jubilant celebrations that followed, 
-                    Yulia was there, camera in hand, ready to capture every fleeting moment of love and joy.</p> 
+                    Hd photography was there, camera in hand, ready to capture every fleeting moment of love and joy.</p> 
                     <p>
                      His ability to blend seamlessly into the background while
                     simultaneously capturing the essence of the moment was nothing short of remarkable. With each click of his camera,
@@ -60,19 +60,19 @@ const Testimonial = () => {
                 <div className='lCont'><img className='love' src='./love.png'/><h1>Jessica  +  Alex</h1></div>
                 <p>When Jessica and Alex began planning their wedding, they knew that finding the right photographer was crucial. 
                     They wanted someone who could capture the essence of their love story – every smile, every tear, every moment of joy. 
-                    That's when they found Yulia, and from the moment they met him, they knew they were in good hands.</p>
+                    That's when they found Hd photography, and from the moment they met him, they knew they were in good hands.</p>
                 <p>
-                On their wedding day, Yulia was more than just a photographer – he was a storyteller, a magician who turned fleeting moments into timeless memories. 
-                From the quiet anticipation before the ceremony to the exhilaration of the first dance, Yulia was there, capturing every precious moment with precision and artistry.</p>
+                On their wedding day, Hd photography was more than just a photographer – he was a storyteller, a magician who turned fleeting moments into timeless memories. 
+                From the quiet anticipation before the ceremony to the exhilaration of the first dance, Hd photography was there, capturing every precious moment with precision and artistry.</p>
             </div>
 
             <div className='testimonial'>
                 <img className='priImg' src='https://image.wedmegood.com/resized-nw/700X/wp-content/uploads/2018/06/DIN2756.jpg' alt='' />
                 <div className='lCont'><img className='love' src='./love.png'/><h1>Emily  +  James</h1></div>
                 <p>When Emily and James embarked on their journey towards forever, they knew they wanted every moment of their wedding day to be cherished for a lifetime.
-                     Enter Yulia, the visionary behind Yulia's Photography – a master of his craft, a wizard with a camera, and a true artist who captures the essence of love with every click.</p>
+                     Enter Hd photography, the visionary behind Hd photography's Photography – a master of his craft, a wizard with a camera, and a true artist who captures the essence of love with every click.</p>
                      <p>
-                     From the first meeting to the final delivery of their wedding album, Yulia's professionalism and passion were evident every step of the way. 
+                     From the first meeting to the final delivery of their wedding album, Hd photography's professionalism and passion were evident every step of the way. 
                     With his keen eye for detail and unwavering commitment to excellence, he transformed fleeting moments into everlasting memories, 
                     each photograph a testament to the beauty and magic of love.</p>
             </div>
@@ -80,10 +80,10 @@ const Testimonial = () => {
             <div className='testimonial'>
                 <img className='priImg' src='https://i.pinimg.com/1200x/72/b7/c9/72b7c9380fa4ff447b15ef3224116155.jpg' alt='' />
                 <div className='lCont'><img className='love' src='./love.png'/><h1>DivYa  +  David</h1></div>
-                <p>Yulia's Photography isn't just about capturing moments – it's about crafting memories that last a lifetime. 
-                    With an eye for detail and a heart full of passion, Yulia transforms ordinary scenes into extraordinary stories, weaving magic into every frame.</p>
+                <p>Hd photography's Photography isn't just about capturing moments – it's about crafting memories that last a lifetime. 
+                    With an eye for detail and a heart full of passion, Hd photography transforms ordinary scenes into extraordinary stories, weaving magic into every frame.</p>
                     <p>
-                    From the first glance to the final dance, Yulia was there, immortalizing the laughter, the tears, and the love that filled the air. His photographs aren't just pictures; 
+                    From the first glance to the final dance, Hd photography was there, immortalizing the laughter, the tears, and the love that filled the air. His photographs aren't just pictures; 
                     they're windows to the soul, capturing the essence of love in its purest form.</p>
             </div>
         </div>
